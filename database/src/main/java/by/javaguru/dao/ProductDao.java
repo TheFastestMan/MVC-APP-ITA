3edbb35723bb49022f0727c86daa7efe6dd7bf68
TheFastestMan <115644047+TheFastestMan@users.noreply.github.com>
2023-09-04 17:20:03 +0700

    Start DB (1)

--- database/src/main/java/by/javaguru/dao/ProductDao.java
+++ database/src/main/java/by/javaguru/dao/ProductDao.java
@@ -1,29 +1,73 @@
 package by.javaguru.dao;
 
 import by.javaguru.entity.Product;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import by.javaguru.exception.DaoException;
+import by.javaguru.util.ConnectionManager;
 
-import java.io.*;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ProductDao {
+    private static final String FIND_ALL_SQL = """
+            SELECT name, quantity FROM products;
+            """;
     private static final String SAVE_SQL = """
-            INSERT INTO flight (
-            flight_no, 
-            departure_date,
-            departure_airport_code, 
-            arrival_date, 
-            arrival_airport_code,
-            aircraft_id,
-            status
-            )
-            VALUES(?,?,?,?,?,?,?);
+            INSERT INTO products (name, quantity)
+            VALUES(?,?);
             """;
     private static final String DELETE_SQL = """
-            DELETE FROM flight WHERE
-            id = ?;
+            DELETE FROM products WHERE
+            name = ?;
             """;
+
+    public List<Product> findAll() {
+        try (var connection = ConnectionManager.open();
+             var prepareStatement = connection.prepareStatement(FIND_ALL_SQL)) {
+            List<Product> seats = new ArrayList<>();
+
+            var result = prepareStatement.executeQuery();
+            while (result.next())
+                seats.add(new Product(
+                                result.getString("name"),
+                                result.getInt("quantity")
+                        )
+                );
+            return seats;
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
+    }
+
+    public Product save(Product product) {
+        try (var connection = ConnectionManager.open();
+             var prepareStatement = connection.prepareStatement(SAVE_SQL,
+                     Statement.RETURN_GENERATED_KEYS)) {
+
+            prepareStatement.setString(1, product.getName());
+            prepareStatement.setInt(2, product.getQuantity());
+
+            prepareStatement.executeUpdate();
+            var keys = prepareStatement.getGeneratedKeys();
+            if (keys.next())
+                product.setId(keys.getLong("id"));
+            return product;
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
+    }
+
+    public boolean delete(String name) {
+
+        try (var connection = ConnectionManager.open();
+             var prepareStatement = connection.prepareStatement(DELETE_SQL)) {
+            prepareStatement.setString(1, name);
+
+            return prepareStatement.executeUpdate() > 0;
+        } catch (SQLException e) {
+            throw new DaoException(e);
+        }
+    }
 }
 // deleteProduct, saveProduct, createNewProduct
--- service/src/main/java/ru/rail/ProductService.java
+++ service/src/main/java/ru/rail/ProductService.java
@@ -4,25 +4,27 @@ import by.javaguru.dao.ProductDao;
 import by.javaguru.entity.Product;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ProductService {
     private ProductDao productDao;
-    private Product product;
 
     public ProductService() throws IOException {
         productDao = new ProductDao();
     }
 
-    public List<Product> getAllProducts() {
-        return productDao.getAllProducts();
-    }
-
     public void addNewProduct(Product product) throws IOException {
-        productDao.createNewProduct(product);
+        productDao.save(product);
     }
 
     public boolean deleteProductByName(String productName) throws IOException {
-        return productDao.deleteProduct(productName);
+        return productDao.delete(productName);
+    }
+
+    public List<Product> getAllProducts() {
+        return productDao.findAll().stream().map(product -> new ProductDto(product.getName(), product.getQuantity())
+        ).collect(Collectors.toList());
     }
 }
